1b73043facaba2b46211eae988dd31990af9f99a
Jaime Santana <jaimesantanal@hotmail.com>
2015-09-07 11:37:32 -0500

    Libreria twig basica, da soporte a templates

--- application/libraries/Twig.php
+++ application/libraries/Twig.php
@@ -1,137 +1,155 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
- * Part of CodeIgniter Simple and Secure Twig
+ * Library to wrap Twig layout engine. Originally from Bennet Matschullat.
+ * Code cleaned up to CodeIgniter standards by Erik Torsner
  *
- * @author     Kenji Suzuki <https://github.com/kenjis>
- * @license    MIT License
- * @copyright  2015 Kenji Suzuki
- * @link       https://github.com/kenjis/codeigniter-ss-twig
+ * PHP Version 5.3
+ *
+ * @category Layout
+ * @package  Twig
+ * @author   Bennet Matschullat <bennet@3mweb.de>
+ * @author   Erik Torsner <erik@torgesta.com>
+ * @license  Don't be a dick http://www.dbad-license.org/
+ * @link     https://github.com/bmatschullat/Twig-Codeigniter
  */
-
-class Twig
-{
-	private $functions_asis = [
-		'base_url', 'site_url'
-	];
-	private $functions_safe = [
-		'form_open', 'form_close', 'form_error', 'set_value', 'form_hidden'
-	];
-
-	private $twig;
-	private $loader;
-
-	public function createTwig()
-	{
-		if (ENVIRONMENT === 'production')
-		{
-			$debug = FALSE;
-		}
-		else
-		{
-			$debug = TRUE;
-		}
-
-		if ($this->loader === null) {
-			$this->loader = new \Twig_Loader_Filesystem([VIEWPATH]);
-		}
-
-		$twig = new \Twig_Environment($this->loader, [
-			'cache'      => APPPATH . '/cache/twig',
-			'debug'      => $debug,
-			'autoescape' => TRUE,
-		]);
-
-		if ($debug)
-		{
-			$twig->addExtension(new \Twig_Extension_Debug());
-		}
-
-		$this->twig = $twig;
-		$this->addCIFunctions();
-	}
-
-	public function setLoader($loader)
-	{
-		$this->loader = $loader;
-	}
-
-	public function render($view, $params = [])
-	{
-		$this->createTwig();
-		$view = $view . '.twig';
-		$CI =& get_instance();
-		$CI->output->set_output($this->twig->render($view, $params));
-	}
-
-	private function addCIFunctions()
-	{
-		// as is functions
-		foreach ($this->functions_asis as $function)
-		{
-			if (function_exists($function))
-			{
-				$this->twig->addFunction(
-					new \Twig_SimpleFunction(
-						$function,
-						$function
-					)
-				);
-			}
-		}
-
-		// safe functions
-		foreach ($this->functions_safe as $function)
-		{
-			if (function_exists($function))
-			{
-				$this->twig->addFunction(
-					new \Twig_SimpleFunction(
-						$function,
-						$function,
-						['is_safe' => ['html']]
-					)
-				);
-			}
-		}
-
-		// customized functions
-		if (function_exists('anchor'))
-		{
-			$this->twig->addFunction(
-				new \Twig_SimpleFunction(
-					'anchor',
-					[$this, 'safe_anchor'],
-					['is_safe' => ['html']]
-				)
-			);
-		}
-	}
-
+/**
+ * Main (and only) class for the Twig wrapper library
+ * 
+ * @category Layout
+ * @package  Twig
+ * @author   Bennet Matschullat <hello@bennet-matschullat.com>
+ * @author   Erik Torsner <erik@torgesta.com>
+ * @license  Don't be a dick http://www.dbad-license.org/
+ * @link     https://github.com/bmatschullat/Twig-Codeigniter
+ */
+class Twig {
+    const 		TWIG_CONFIG_FILE = 'twig';
 	/**
-	 * @param string $uri
-	 * @param string $title
-	 * @param array $attributes [changed] only array is acceptable
-	 * @return string
+	 * Path to templates. Usually application/views.
+	 * 
+	 * @var string
 	 */
-	public function safe_anchor($uri = '', $title = '', $attributes = [])
-	{
-		$uri = html_escape($uri);
-		$title = html_escape($title);
-		
-		$new_attr = [];
-		foreach ($attributes as $key => $val)
-		{
-			$new_attr[html_escape($key)] = html_escape($val);
-		}
-
-		return anchor($uri, $title, $new_attr);
-	}
-
+	protected $template_dir;
 	/**
-	 * @return \Twig_Environment
+	 * Path to cache.  Usually applcation/cache.
+	 * 
+	 * @var string
 	 */
-	public function getTwig()
+	protected $cache_dir;
+	/**
+	 * Reference to code CodeIgniter instance.
+	 * 
+	 * @var CodeIgniter object
+	 */
+	private $_ci;
+	/**
+	 * Twig environment see http://twig.sensiolabs.org/api/v1.8.1/Twig_Environment.html.
+	 * 
+	 * @var Twig_Envoronment object
+	 */
+	private $_twig_env;
+	/**
+	 * constructor of twig ci class
+	 */
+	public function __construct() 
 	{
-		return $this->twig;
+		$this->_ci = & get_instance();
+		$this->_ci->config->load(self::TWIG_CONFIG_FILE); // load config file
+		// set include path for twig
+		ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . APPPATH . 'third_party/Twig/lib/Twig');
+		require_once (string)'Autoloader.php';
+		// register autoloader
+		Twig_Autoloader::register();
+		log_message('debug', 'twig autoloader loaded');
+		// init paths
+		$this->template_dir = $this->_ci->config->item('template_dir');
+		$this->cache_dir = $this->_ci->config->item('cache_dir');
+		// load environment
+		$loader = new Twig_Loader_Filesystem($this->template_dir, $this->cache_dir);
+		$this->_twig_env = new Twig_Environment($loader, array(
+			'cache' => $this->cache_dir,
+			'auto_reload' => TRUE));
+		$this->ci_function_init();
+	}
+	/**
+	 * render a twig template file
+	 * 
+	 * @param string  $template template name
+	 * @param array   $data	    contains all varnames
+	 * @param boolean $render   render or return raw?
+	 *
+	 * @return void
+	 * 
+	 */
+	public function render($template, $data = array(), $render = TRUE) 
+	{
+		$template = $this->_twig_env->loadTemplate($template);
+		log_message('debug', 'twig template loaded');
+		return ($render) ? $template->render($data) : $template;
+	}
+	/**
+	 * Execute the template and send to CI output
+	 * 
+	 * @param string $template Name of template
+	 * @param array  $data     Parameters for template
+	 * 
+	 * @return void
+	 * 
+	 */
+	public function display($template, $data = array()) 
+	{
+		$template = $this->_twig_env->loadTemplate($template);
+		$this->_ci->output->set_output($template->render($data));
+	}
+	/**
+	 * Entry point for controllers (and the likes) to register
+	 * callback functions to be used from Twig templates
+	 * 
+	 * @param string                 $name     name of function
+	 * @param Twig_FunctionInterface $function Function pointer
+	 * 
+	 * @return void
+	 * 
+	 */
+	public function register_function($name, Twig_FunctionInterface $function) 
+	{
+		$this->_twig_env->addFunction($name, $function);
+	}
+	/**
+	 * Initialize standard CI functions
+	 * 
+	 * @return void
+	 */
+	public function ci_function_init() 
+	{
+		$this->_twig_env->addFunction('base_url', new Twig_Function_Function('base_url'));
+		$this->_twig_env->addFunction('site_url', new Twig_Function_Function('site_url'));
+		$this->_twig_env->addFunction('current_url', new Twig_Function_Function('current_url'));
+		// form functions
+		$this->_twig_env->addFunction('form_open', new Twig_Function_Function('form_open'));
+		$this->_twig_env->addFunction('form_hidden', new Twig_Function_Function('form_hidden'));
+		$this->_twig_env->addFunction('form_input', new Twig_Function_Function('form_input'));
+		$this->_twig_env->addFunction('form_password', new Twig_Function_Function('form_password'));
+		$this->_twig_env->addFunction('form_upload', new Twig_Function_Function('form_upload'));
+		$this->_twig_env->addFunction('form_textarea', new Twig_Function_Function('form_textarea'));
+		$this->_twig_env->addFunction('form_dropdown', new Twig_Function_Function('form_dropdown'));
+		$this->_twig_env->addFunction('form_multiselect', new Twig_Function_Function('form_multiselect'));
+		$this->_twig_env->addFunction('form_fieldset', new Twig_Function_Function('form_fieldset'));
+		$this->_twig_env->addFunction('form_fieldset_close', new Twig_Function_Function('form_fieldset_close'));
+		$this->_twig_env->addFunction('form_checkbox', new Twig_Function_Function('form_checkbox'));
+		$this->_twig_env->addFunction('form_radio', new Twig_Function_Function('form_radio'));
+		$this->_twig_env->addFunction('form_submit', new Twig_Function_Function('form_submit'));
+		$this->_twig_env->addFunction('form_label', new Twig_Function_Function('form_label'));
+		$this->_twig_env->addFunction('form_reset', new Twig_Function_Function('form_reset'));
+		$this->_twig_env->addFunction('form_button', new Twig_Function_Function('form_button'));
+		$this->_twig_env->addFunction('form_close', new Twig_Function_Function('form_close'));
+		$this->_twig_env->addFunction('form_prep', new Twig_Function_Function('form_prep'));
+		$this->_twig_env->addFunction('set_value', new Twig_Function_Function('set_value'));
+		$this->_twig_env->addFunction('set_select', new Twig_Function_Function('set_select'));
+		$this->_twig_env->addFunction('set_checkbox', new Twig_Function_Function('set_checkbox'));
+		$this->_twig_env->addFunction('set_radio', new Twig_Function_Function('set_radio'));
+		$this->_twig_env->addFunction('form_open_multipart', new Twig_Function_Function('form_open_multipart'));
 	}
 }
+/* End of file Twig.php */
+/* Location: ./libraries/Twig.php */
