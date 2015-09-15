4a28d1d9188e99337bf9e8ec4bff14717977ea86
Mario Torres <mariofertc@mixmail.com>
2015-09-15 12:24:38 -0500

    integración de twig y hmvc CI3

--- application/libraries/Twig.php
+++ application/libraries/Twig.php
@@ -10,17 +10,24 @@
 
 class Twig
 {
-	private $functions_asis = [
+	/*private $functions_asis = [
 		'base_url', 'site_url'
 	];
 	private $functions_safe = [
 		'form_open', 'form_close', 'form_error', 'set_value', 'form_hidden'
-	];
+	];*/
 
 	private $twig;
 	private $loader;
+	private $CI;
+	private $_template_locations = array();
+	private $_themes_base_dir;
+	private $_module;
+	private $_globals = array();
+	private $_data = array();
+	private $config;
 
-	public function createTwig()
+	public function __construct()
 	{
 		if (ENVIRONMENT === 'production')
 		{
@@ -30,9 +37,14 @@ class Twig
 		{
 			$debug = TRUE;
 		}
+		$this->CI =& get_instance();
+		$this->config = $this->CI->config->item('twig');
+		$this->_themes_base_dir = APPPATH . $this->config['themes_base_dir'];
+		$this->setTemplateLocations($this->config['default_theme']);
 
 		if ($this->loader === null) {
-			$this->loader = new \Twig_Loader_Filesystem([VIEWPATH]);
+			$this->loader = new \Twig_Loader_Filesystem($this->_template_locations);
+//			$this->loader = new \Twig_Loader_Filesystem([VIEWPATH]);
 		}
 
 		$twig = new \Twig_Environment($this->loader, [
@@ -45,9 +57,78 @@ class Twig
 		{
 			$twig->addExtension(new \Twig_Extension_Debug());
 		}
-
 		$this->twig = $twig;
+		
+		$this->theme($this->config['default_theme'])
+			 ->layout($this->config['default_layout']);
+			 //->template($this->_config['default_template']);
+
 		$this->addCIFunctions();
+	}
+	
+	public function theme($theme)
+	{
+		if(!is_dir(realpath($this->_themes_base_dir. $theme)))
+		{
+			log_message('error', 'Twiggy: requested theme '. $theme .' has not been loaded because it does not exist.');
+			show_error("Theme does not exist in {$this->_themes_base_dir}{$theme}.");
+		}
+
+		$this->_theme = $theme;
+		$this->setTemplateLocations($theme);
+
+		return $this;
+	}
+	public function layout($name)
+	{
+		$this->_layout = $name;
+		//$this->_twig->addGlobal('_layout', '_layouts/'. $this->_layout . $this->config['template_file_ext']);
+		$this->twig->addGlobal('_layout', '_layouts/'. $this->_layout . $this->config['template_file_ext']);
+
+		return $this;
+	}
+	
+	/**
+	* Set template locations
+	*
+	* @access	private
+	* @param 	string	name of theme to load
+	* @return	void
+	*/
+	private function setTemplateLocations($theme)
+	{
+		// Reset template locations array since we loaded a different theme
+		$this->_template_locations = array();
+		
+		// Check if HMVC is installed.
+		// NOTE: there may be a simplier way to check it but this seems good enough.
+		if(method_exists($this->CI->router, 'fetch_module'))
+		{
+			$this->_module = $this->CI->router->fetch_module();
+
+			// Only if the current page is served from a module do we need to add extra template locations.
+			if(!empty($this->_module))
+			{
+				$module_locations = Modules::$locations;
+
+				foreach($module_locations as $loc => $offset)
+				{
+					/* Only add the template location if the same exists, otherwise
+					you'll need always a directory for your templates, even your module
+					won't use templates */
+					if ( is_dir($loc . $this->_module . '/' . $this->config['themes_base_dir'] . $theme) )
+						$this->_template_locations[] = $loc . $this->_module . '/' . $this->config['themes_base_dir'] . $theme;
+				}
+			}
+		}
+
+		$this->_template_locations[] =  $this->_themes_base_dir . $theme;
+
+		// Reset the paths if needed.
+		if(is_object($this->loader))
+		{
+			$this->loader->setPaths($this->_template_locations);
+		}
 	}
 
 	public function setLoader($loader)
@@ -56,17 +137,19 @@ class Twig
 	}
 
 	public function render($view, $params = [])
-	{
-		$this->createTwig();
-		$view = $view . '.twig';
+	{ 
+	//	$this->createTwig();
+		$params = array_merge($this->_data,$params);
+		$view = $view . $this->config['template_file_ext'];
 		$CI =& get_instance();
+		//$CI->output->set_output($this->_load()->render($view, $params));
 		$CI->output->set_output($this->twig->render($view, $params));
 	}
 
 	private function addCIFunctions()
 	{
 		// as is functions
-		foreach ($this->functions_asis as $function)
+		foreach ($this->config['functions_asis'] as $function)
 		{
 			if (function_exists($function))
 			{
@@ -80,7 +163,7 @@ class Twig
 		}
 
 		// safe functions
-		foreach ($this->functions_safe as $function)
+		foreach ($this->config['functions_safe'] as $function)
 		{
 			if (function_exists($function))
 			{
@@ -133,5 +216,37 @@ class Twig
 	public function getTwig()
 	{
 		return $this->twig;
+	}
+	
+	/**
+	 * Set data
+	 * 
+	 * @access	public
+	 * @param 	mixed  	key (variable name) or an array of variable names with values
+	 * @param 	mixed  	data
+	 * @param 	boolean	(optional) is this a global variable?
+	 * @return	object 	instance of this class
+	 */
+
+	public function set($key, $value, $global = FALSE)
+	{
+		if(is_array($key))
+		{
+			foreach($key as $k => $v) $this->set($k, $v, $global);
+		}
+		else
+		{
+			if($global)
+			{
+				$this->twig->addGlobal($key, $value);
+				$this->_globals[$key] = $value;
+			}
+			else
+			{
+			 	$this->_data[$key] = $value;
+			}	
+		}
+
+		return $this;
 	}
 }
